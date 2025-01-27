90e404a183c925699e209e741ec4b2e733fdc294
Dani Vals <daniel.vals.simon@gmail.com>
2025-01-27 12:03:43 +0100

    FEAT: plantillas para iniciar sesion y crear cuenta con CSS metido

---
+++ src/Controller/TestingDELETE.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Categoria;
+use App\Entity\Producto;
+use App\Entity\Pedido;
+use App\Entity\PedidoProducto;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+class TestingDELETE extends AbstractController
+{
+	#[Route('/feed', name:'feed')]
+    public function loadFeed() {
+
+		return $this->render("feed.html.twig");
+    }
+	#[Route('/iniciasesion', name:'inicio_sesion')]
+    public function loadIS() {
+
+		return $this->render("singin.html.twig");
+    }
+}
