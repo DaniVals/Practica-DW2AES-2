9602f0b231f7fe13ea115aa2c40d49761e624988
Iván Arroyo González <ivanag2005ag@gmail.com>
2025-02-05 13:59:49 +0100

    fix: date time fix

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -9,28 +9,28 @@ use Doctrine\ORM\Mapping as ORM;
 class Comment
 {
 	#[ORM\Id]
-    #[ORM\Column(type:'integer', name:'idComment')]
-    #[ORM\GeneratedValue]
-    private $idComment;
+	#[ORM\Column(type:'integer', name:'idComment')]
+	#[ORM\GeneratedValue]
+	private $idComment;
 
 	#[ORM\Column(type:'integer', name:'commentedPost')]
-    private $commentedPost;
+	private $commentedPost;
 
 	#[ORM\Column(type:'integer', name:'commentedComment')]
 	// onetomany post
-    private $commentedComment;
+	private $commentedComment;
 
 	#[ORM\Column(type:'string', name:'content')]
-    private $content;
+	private $content;
 
 	#[ORM\Column(type:'integer', name:'likes')]
-    private $likes;
+	private $likes;
 
 	#[ORM\Column(type:'integer', name:'dislikes')]
-    private $dislikes;
+	private $dislikes;
 
-	#[ORM\Column(type:'Types::DATETIME_IMMUTABLE', name:'postingTime')]
-    private $postingTime;
+	#[ORM\Column(type:'datetime_immutable', name:'postingTime')]
+	private $postingTime;
 
 //-----------------------------------------------------------
 
