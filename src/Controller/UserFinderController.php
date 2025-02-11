696e9b280cc152807dca894dd2bd1bc10bcf76ff
Iván Arroyo González <ivanag2005ag@gmail.com>
2025-02-11 13:55:14 +0100

    feat: user finder by letters

--- src/Controller/UserFinderController.php
+++ src/Controller/UserFinderController.php
@@ -10,12 +10,37 @@ use App\Entity\Profile;
 use App\Entity\Comment;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFinderController extends AbstractController {
+
+    #[Route('/find', name:'load_finder')]
+    public function load_finder(EntityManagerInterface $entityManager) {
+        $users = $entityManager->getRepository(User::class)->findAll();
+        return $this->render('navigation/find_user.html.twig', ['users' => $users]);
+    }
+
+    // Cada vez que se escriba una letra, JS hará una petición al servidor para buscar usuarios que coincidan con la o las letras
+    // Se mostrará una lista de usuarios que empiezen con la o las letras
+    #[Route('/find', name:'find_user')]
+    public function find_user(EntityManagerInterface $entityManager, Request $request) {
+        // Devolver un JSON con los usuarios que coincidan con la búsqueda
+        $search = $request->get('search');
+        $profiles = $entityManager->getRepository(Profile::class)->createQueryBuilder('u')
+            ->where('u.userName LIKE :search')
+            ->setParameter('search', $search . '%')
+            ->getQuery()
+            ->getResult();
+        $usernames = [];
+        foreach ($profiles as $profile) {
+            $usernames[] = $profile->getUserName();
+        }
+        return new JsonResponse($usernames);  
+    }
 
     #[Route('/find/{username}', name:'load_user')]
     public function load_user($username, EntityManagerInterface $entityManager) {
