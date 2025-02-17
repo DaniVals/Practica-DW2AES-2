926e42f81afdc37e5ccfd605ac74926b5b5a94cf
Iván Arroyo González <ivanag2005ag@gmail.com>
2025-02-17 13:35:46 +0100

    feat: friend post

--- src/Controller/PostLoader.php
+++ src/Controller/PostLoader.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use App\Entity\Role;
 use App\Entity\Post;
 use App\Entity\Profile;
+use App\Entity\Friendship;
 use App\Entity\Comment;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,7 +18,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 
 class PostLoader extends AbstractController {
-    
+
     #[Route('/loadPost', name:'load_post_ajax')]
     public function loadPost(EntityManagerInterface $entityManager, Request $request) {
         if (!$request->isXmlHttpRequest()) {
@@ -34,21 +35,74 @@ class PostLoader extends AbstractController {
         }
         return new JsonResponse($post_arr);
     }
+
+#[Route('/loadPost/timeline', name: 'load_post_timeline_ajax')]
+public function loadPostTimeline(EntityManagerInterface $entityManager, Request $request): JsonResponse
+{
+    if (!$request->isXmlHttpRequest()) {
+        return $this->redirectToRoute('feed');
+    }
+
+    $user = $this->getUser();
+    if (!$user) {
+        return new JsonResponse(['error' => 'User not authenticated'], Response::HTTP_UNAUTHORIZED);
+    }
+
+    // Obtener los IDs de los amigos confirmados
+    $friends = $entityManager->createQueryBuilder()
+        ->select('CASE 
+                    WHEN f.IdRequestor = :userId THEN u2.idUser
+                    ELSE u1.idUser
+                  END as friendId')
+        ->from(Friendship::class, 'f')
+        ->leftJoin('f.IdRequestor', 'u1')
+        ->leftJoin('f.IdRequested', 'u2')
+        ->where('(f.IdRequestor = :userId OR f.IdRequested = :userId)')
+        ->andWhere('f.frState = 2')  
+        ->setParameter('userId', $this->getUser())
+        ->getQuery()
+        ->getResult();
+
+    $friendIds = array_column($friends, 'friendId');
+
+    if (empty($friendIds)) {
+        return new JsonResponse([]);
+    }
+
+    // Obtener los posts de los amigos confirmados
+    $posts = $entityManager->createQueryBuilder()
+        ->select('p')
+        ->from(Post::class, 'p')
+        ->where('p.PosterProfile IN (:friendIds)')
+        ->setParameter('friendIds', $friendIds)
+        ->orderBy('p.postingTime', 'DESC')
+        ->setMaxResults(10)
+        ->getQuery()
+        ->getResult();
+
+    $post_arr = [];
+    foreach ($posts as $post) {
+        $post_arr[] = $post->getPostInfoForAJAX();
+    }
+
+    return new JsonResponse($post_arr);
+}
+
     #[Route('/loadPost/user/{userId}', name:'load_post_user_ajax')]
     public function loadPostUser($userId, EntityManagerInterface $entityManager, Request $request) {
         if (!$request->isXmlHttpRequest()) {
             return $this->redirectToRoute('feed');
         }
-		$profile = $entityManager->getRepository(Profile::class)->findOneBy(['idUser' => $userId]);
-		$posts = $entityManager->getRepository(Post::class)->findBy(['PosterProfile' => $profile]);
-		$post_arr = [];
-		// $getWatched = $request->query->get('watched');
-		foreach ($posts as $post) {
-			// if ($post->getIdPost() <= $getWatched) {
-			//     continue;
-			// }
-			$post_arr[] = $post->getPostInfoForAJAX();
-		}
-		return new JsonResponse($post_arr);
-	}
+        $profile = $entityManager->getRepository(Profile::class)->findOneBy(['idUser' => $userId]);
+        $posts = $entityManager->getRepository(Post::class)->findBy(['PosterProfile' => $profile]);
+        $post_arr = [];
+        // $getWatched = $request->query->get('watched');
+        foreach ($posts as $post) {
+            // if ($post->getIdPost() <= $getWatched) {
+            //     continue;
+            // }
+            $post_arr[] = $post->getPostInfoForAJAX();
+        }
+        return new JsonResponse($post_arr);
+    }
 }
